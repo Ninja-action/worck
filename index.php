3025b3a9e3b7b2bec3efaff1a81241ff33d5a313
sisadmin2 <sisadmin2@sisadmin-2.tabakvintorg.gml.local>
2015-06-30 11:26:48 +0300

    * Реализовал подобие верхнего меню; * Обновил jquery.keyboard.js - v1.23.1; * Обновил buzz.min.js - v1.1.10; * Обновил jquery.min.js - v2.1.4; * Обновил pixi.js - v2.2.7; * Упорядочил файлы; * Добавил картинки (вкл/отк музыка, завод );

--- index.php
+++ index.php
@@ -1,47 +1,20 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <meta charset="utf-8" />
-        <!--[if lt IE 9]><script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
-        <title>Ninja Action</title>
-        <style>
-            body {
-                margin: 0;
-                padding: 0;
-                background-color: #000000;
-                overflow: hidden;
-            }
-            .game{
-                width:100%;
-                cursor: pointer;                
-            }
-            .conteiner{
-                width:100%;
-                float: left;
-            }
-            .info{
-                color:#fff;
-            }
-            .comments{
-                float: right;
-                //width:400px;
-                height: 500px;
-                overflow: scroll;
-            }
-            .clear{
-                clear: both;
-            }
-        </style>
-        <script type="text/javascript" src="jquery.js"></script>
-        <script type="text/javascript" src="buzz.min.js"></script>
-        <script type="text/javascript" src="jquery.keyboard.js"></script>
-        <script src="pixi.js"></script>   
-        <script src="box2d.min.js"></script>
+        <meta charset="utf-8"/>
+         <link rel="stylesheet" type="text/css" href="css/menu.css">
+        <title>Ninja Action</title>    
+         <script type="text/javascript" src="js/libs/jquery.min.js"></script>
+         <script type="text/javascript" src="js/libs/buzz.min.js"></script>
+         <script type="text/javascript" src="js/libs/jquery.keyboard.js"></script>
+         <script src="js/libs/pixi.js"></script>   
+         <script src="js/libs/box2d.min.js"></script>
 
         <script src="classes/fons.js"></script>
         <script src="classes/player.js"></script>
 
-        <script src="objects/level_1.js"></script>
+        
+        <script src="js/levels/level_1.js"></script>
 
 
         <script src="myminigame.js"></script>
@@ -49,26 +22,15 @@
     </head>
 
     <body>
-        <!--        <div class="comments">
-                     Put this script tag to the <head> of your page 
-                    <script type="text/javascript" src="//vk.com/js/api/openapi.js?116"></script>
-        
-                    <script type="text/javascript">
-                        VK.init({apiId: 4736921, onlyWidgets: true});
-                    </script>
-        
-                     Put this div tag to the place, where the Comments block will be 
-                    <div id="vk_comments"></div>
-                    <script type="text/javascript">
-                        VK.Widgets.Comments("vk_comments", {limit: 5, width: "400", attach: "*"});
-                    </script>
-                </div>-->
-        <div class="conteiner">
-            <div  class="game">
-            </div>
-            <div class="info">
-                <button id="stop">Вырубить музыку</button>
-                <button id="play">Врубить музыку</button>
+        <ul class="menu">
+            <li><a id="play"><img src="images/common/muzikon.png"></a></li>
+            <li><a id="stop"><img src="images/common/muzikoff.png"></a></li>    
+            <li><a id="stop"><img src="images/common/help.png"></a></li>
+	    <li><a href="http://www.youtube.com/id22066662"><img src="images/common/youtubeicon.png"></a></li>
+        </ul>
+
+            <div  class="game"></div>
+            <!--<div class="info">
                 <p>Влево: A или стрелка</p>
                 <p>Вправо: D или стрелка</p>
                 <p>Удар: V</p>
@@ -77,12 +39,7 @@
                 <p>Забраться на стену: Space</p>
             </div>
         </div>
-        <div class="clear"></div>
-
-
-        <a target="_blank" href="http://www.youtube.com/id22066662">Ссылка на канал автора=)</a>
-
-
+        <div class="clear"></div>--> 
 
     </body>
 </html>
--- js/levels/level_1.js
+++ js/levels/level_1.js
@@ -1,4 +1,5 @@
 var fons = new Fons();
+//Главный фон
 function load_level_1() {
     fons.images.push({
         'image': {
@@ -10,9 +11,10 @@ function load_level_1() {
             'physics': false,
             'position': {'x': 0, 'y': 0},
             'anchor': {'x': 0, 'y': 0},
-            'object': new PIXI.Texture.fromImage('fon.jpg')
+            'object': new PIXI.Texture.fromImage('images/levels/1/fon.jpg')         
         }
     });
+//Тратуар     
     fons.images.push({
         'image': {
             'name': 'pavement',
@@ -27,9 +29,10 @@ function load_level_1() {
             },
             'position': {'x': 0, 'y': window.h-100*0.3-10},
             'anchor': {'x': 0, 'y': 0.3},
-            'object': new PIXI.Texture.fromImage('pavement.jpg')
+            'object': new PIXI.Texture.fromImage('images/levels/1/pavement.jpg')
         }
     });
+//Здание-1    
     fons.images.push({
         'image': {
             'name': 'home',
@@ -40,9 +43,10 @@ function load_level_1() {
             'physics': false,
             'position': {'x': 0, 'y': window.h-810-70},
             'anchor': {'x': 0, 'y': 0},
-            'object': new PIXI.Texture.fromImage('home.jpg')
+            'object': new PIXI.Texture.fromImage('images/levels/1/home.jpg')
         }
     });
+//Здание-2    
     fons.images.push({
         'image': {
             'name': 'priton',
@@ -53,9 +57,24 @@ function load_level_1() {
             'physics': false,
             'position': {'x': 2100, 'y': window.h-1125-60},
             'anchor': {'x': 0, 'y': 0},
-            'object': new PIXI.Texture.fromImage('priton.png')
+            'object': new PIXI.Texture.fromImage('images/levels/1/priton.png')
         }
     });
+//Здание-3   
+    fons.images.push({
+        'image': {
+            'name': 'zavod',
+            'width': 792,
+            'height': 481,
+            'ground': false,
+            'static': false,
+            'physics': false,
+            'position': {'x': 5000, 'y': window.h-480-65},
+            'anchor': {'x': 0, 'y': 0},
+            'object': new PIXI.Texture.fromImage('images/levels/1/zavod.jpg')
+        }
+    });
+//Дерево    
     fons.images.push({
         'image': {
             'name': 'tree1',
@@ -66,9 +85,10 @@ function load_level_1() {
             'physics': false,
             'position': {'x': 0, 'y': window.h-865-50},
             'anchor': {'x': 0, 'y': 0},
-            'object': new PIXI.Texture.fromImage('tree1.png')
+            'object': new PIXI.Texture.fromImage('images/levels/1/tree1.png')
         }
     });
+//Скамейка    
     fons.images.push({
         'image': {
             'name': 'skameika',
@@ -79,9 +99,10 @@ function load_level_1() {
             'physics': false,
             'position': {'x': 0, 'y': window.h-83-50},
             'anchor': {'x': 0, 'y': 0},
-            'object': new PIXI.Texture.fromImage('skameika.png')
+            'object': new PIXI.Texture.fromImage('images/levels/1/skameika.png')
         }
     });
+//Шарик?    
     fons.images.push({
         'image': {
             'name': 'circle',
@@ -99,6 +120,7 @@ function load_level_1() {
             'object': new PIXI.Texture.fromImage('circle.png')
         }
     });
+//Хрень?    
     fons.images.push({
         'image': {
             'name': 'wall',
@@ -117,6 +139,7 @@ function load_level_1() {
             'object': new PIXI.Texture.fromImage('wall.png')
         }
     });
+//Хрень2?
     fons.images.push({
         'image': {
             'name': 'wall2',
@@ -128,11 +151,10 @@ function load_level_1() {
             'physics_function': function (object) {
                 //addBodyC(object, 100, 410, 50, 50, 0.5);
                 // return false;
-
             },
             'position': {'x': 900, 'y': window.h-210-40},
             'anchor': {'x': 0, 'y': 0},
-            'object': new PIXI.Texture.fromImage('static/wall2.jpg')
+            'object': new PIXI.Texture.fromImage('wall2.jpg')
         }
     });
     fons.setTilingSprite();
